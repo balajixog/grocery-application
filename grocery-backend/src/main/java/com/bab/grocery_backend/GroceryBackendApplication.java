f839f55f0aa1d51bf012b617e967f8af8b23ec92
Balaji G <balaji31072005@gmail.com>
2026-02-07 22:45:14 +0530

    feat: implement JWT authentication
    - Add JWT token creation and expiration handling
    - Implement email extraction from JWT tokens
    - Configure JWT security filter chain

--- grocery-backend/src/main/java/com/bab/grocery_backend/GroceryBackendApplication.java
+++ grocery-backend/src/main/java/com/bab/grocery_backend/GroceryBackendApplication.java
@@ -3,11 +3,20 @@ package com.bab.grocery_backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class GroceryBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GroceryBackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure()
+                .ignoreIfMissing()
+                .load();
 
+        dotenv.entries().forEach(e ->
+                System.setProperty(e.getKey(), e.getValue())
+        );
+
+        SpringApplication.run(GroceryBackendApplication.class, args);
+    }
 }
---
+++ grocery-backend/src/main/java/com/bab/grocery_backend/security/SecurityConfig.java
@@ -0,0 +1,55 @@
+package com.bab.grocery_backend.security;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import com.bab.grocery_backend.util.JwtFilter;
+
+@Configuration
+public class SecurityConfig {
+        
+
+        @Bean
+        public PasswordEncoder passwordEncoder() throws Exception{    //use encode and matches the validation 
+                return new BCryptPasswordEncoder();
+        }
+        @Bean
+        public SecurityFilterChain filterChain(HttpSecurity http,JwtFilter jwtFilter) throws Exception{
+            http
+                 .csrf(AbstractHttpConfigurer::disable)
+                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                 .authorizeHttpRequests((auth)-> auth
+                 .requestMatchers("/auth/**").permitAll()
+                .anyRequest().authenticated()
+              )
+              
+                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+                 
+                return http.build();
+        }
+
+        @Bean
+        public CorsConfigurationSource corsConfigurationSource(){
+                CorsConfiguration configuration=new CorsConfiguration();
+                configuration.setAllowedOrigins(List.of("*"));   //replace with website url in the furture
+                configuration.setAllowedMethods(List.of("GET","POST","PUT","DELETE"));
+                configuration.setAllowedHeaders(List.of("*"));
+
+                UrlBasedCorsConfigurationSource source= new UrlBasedCorsConfigurationSource();
+                source.registerCorsConfiguration("/**", configuration);
+
+                return source;
+        }
+    
+}
