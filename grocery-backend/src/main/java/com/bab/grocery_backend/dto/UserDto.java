0377478756a8d1228838b8f6259f4a26b7e72a39
Balaji G <balaji31072005@gmail.com>
2026-02-08 17:40:20 +0530

    feat: add login, registration, roles, and JWT security

--- grocery-backend/src/main/java/com/bab/grocery_backend/dto/UserDto.java
+++ grocery-backend/src/main/java/com/bab/grocery_backend/dto/UserDto.java
@@ -1,12 +1,17 @@
 package com.bab.grocery_backend.dto;
 
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class UserDto {
     @Email
     String email;
-    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$\n")
-    String password;
+    String role;
 }
