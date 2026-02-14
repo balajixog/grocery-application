24d38c418490acf7fb648677037b2d374a1fdeae
Balaji G <balaji31072005@gmail.com>
2026-02-14 23:58:28 +0530

    fix:category

--- grocery-backend/src/main/java/com/bab/grocery_backend/service/CategoryServiceImpl.java
+++ grocery-backend/src/main/java/com/bab/grocery_backend/service/CategoryServiceImpl.java
@@ -15,16 +15,22 @@ public class CategoryServiceImpl implements CategoryService {
     private final CategoryRepository categoryRepository;
 
     @Override
-    public CategoryResponseDto createCategory(CreateCategoryRequestDto dto) {
-        if(categoryRepository.existsByName(dto.getName())){
-            throw new RuntimeException("Category already exists");
-        }
-        Category category = Category.builder()
-                            .name(dto.getName())
-                            .build();
-        Category save =categoryRepository.save(category);
-        return  new CategoryResponseDto(save.getId(), save.getName());
+public CategoryResponseDto createCategory(CreateCategoryRequestDto dto) {
 
+    if (categoryRepository.existsByName(dto.getName())) {
+        throw new RuntimeException("Category already exists");
     }
+
+    Category category = Category.builder()   
+            .name(dto.getName())
+            .build();
+
+    Category savedCategory = categoryRepository.save(category);
+
+    return new CategoryResponseDto(
+            savedCategory.getId(),
+            savedCategory.getName()
+    );
+}
     
 }
